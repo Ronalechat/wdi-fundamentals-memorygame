41eb51b14815b998f2b7d50635da99325db5f290
Pinky <dir.michaellin@gmail.com>
2016-09-02 15:59:16 +1000

    "Added JS - pop up test for matches"

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,12 @@
 /*console.log("JS file is connected to HTML! Woo!") */
+//Variables for cards. Tested for memory. 
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+	if (cardTwo === cardFour) {
+	window.alert("You got a match!");
+}	else {
+	window.alert("no match, try again.");
+}
